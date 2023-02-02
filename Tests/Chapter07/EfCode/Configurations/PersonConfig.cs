ea79e407609c0eaa15a299581575fae3d5b907f3
Нурдаулет Хожакельдиев <khozhakeldiyev_n@bi.group>
2023-02-02 20:08:16 +0600

    Ended chapter7

---
+++ Tests/Chapter07/EfCode/Configurations/PersonConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tests.Chapter07.EfClasses;
+
+namespace Tests.Chapter07.EfCode.Configurations
+{
+    public class PersonConfig : IEntityTypeConfiguration<Person>
+    {
+        // Here I use alternate key whick person.UserId. 
+        // ContactInfo.EmailAddress is foreign key to person.UserId
+        // Alternate key = principal key
+        public void Configure(EntityTypeBuilder<Person> entity)
+        {
+            entity
+                .HasOne(p => p.ContactInfo)
+                .WithOne()
+                .HasForeignKey<ContactInfo>(p => p.EmailAddress)
+                .HasPrincipalKey<Person>(p => p.UserId);
+        }
+    }
+}
